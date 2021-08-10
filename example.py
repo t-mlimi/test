e36a210df45f19d005b1f1a9fbcf45a92f6b60f0
t-mlimi <terence.mlimi@takealot.com>
2021-08-10 22:49:48 +0200

    pytest

--- example.py
+++ example.py
@@ -1,16 +1,10 @@
-import math
-# Assign values to x and n
-x = 4
-n = 3
-
-# Method 1
-power = x ** n
-print("%d to the power %d is %d" % (x,n,power))
-
-# Method 2
-power = pow(x,n)
-print("%d to the power %d is %d" % (x,n,power))
-
-# Method 3
-power = math.pow(2,6.5)
-print("%d to the power %d is %5.2f" % (x,n,power))
+import pytest
+def test_file1_method1():
+	x=5
+	y=6
+	assert x+1 == y,"test failed"
+	assert x == y,"test failed"
+def test_file1_method2():
+	x=5
+	y=6
+	assert x+1 == y,"test failed"
