4767da78b6d4312849538eb00bfefe16c1684478
t-mlimi <terence.mlimi@takealot.com>
2022-03-16 14:09:49 +0200

    added comments for login test

--- test_login_user.py
+++ test_login_user.py
@@ -3,7 +3,7 @@ import requests
 import json
 import allure
 
-
+#testing valid login
 def test_login_valid(supply_url):
 	url = supply_url + "/login/" 
 	data = {'email':'eve.holt@reqres.in','password':'cityslicka'}
@@ -13,6 +13,7 @@ def test_login_valid(supply_url):
 	assert j['token'] == "QpwL5tke4Pnpja7X4", resp.text
 
 
+#testing login with no password
 def test_login_no_password(supply_url):
 	url = supply_url + "/login/" 
 	data = {'email':'test@test.com'}
@@ -22,6 +23,7 @@ def test_login_no_password(supply_url):
 	assert j['error'] == "Missing password", resp.text
 
 
+# testing login with no email
 def test_login_no_email(supply_url):
 	url = supply_url + "/login/" 
 	data = {}
